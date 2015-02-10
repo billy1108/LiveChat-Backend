66c4782b10bae0e9d4480f1cedbd69dd4422b3b2
billy.1108 <billy.11080@gmail.com>
2015-02-10 13:59:42 -0500

    add passport

--- index.js
+++ index.js
@@ -12,12 +12,39 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
+var passport = require('passport'), FacebookStrategy = require('passport-facebook').Strategy;
+
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
 
+// passport
+
+passport.use(new FacebookStrategy({
+    clientID: "1545227259086169",
+    clientSecret: "1cb30a7565c8ecdc367e1f222f62035b",
+    callbackURL: "http://livechat-multiplatform.herokuapp.com/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    console.log("username ==> "+ profile.username)
+  }
+));
+
 // Routing
 app.use(express.static(__dirname + '/public'));
+
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook', passport.authenticate('facebook',{scope:'email'}));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' }));
 
 // Chatroom
 
@@ -25,6 +52,11 @@ app.use(express.static(__dirname + '/public'));
 var usernames = {};
 var numUsers = 0;
 
+
+
+
+
+// handle Sockets
 io.on('connection', function (socket) {
   console.log("entre");
   var addedUser = false;
